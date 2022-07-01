ae536fcd39af19ec1d16245aeeae4f1c273d31c6
smooti <kobeowens43099@gmail.com>
2022-07-01 05:16:39 -0400

    formatting

--- CLIController.ps1
+++ CLIController.ps1
@@ -1,49 +1,49 @@
 #requires -module SmootiTools
 
 do {
-    Write-Host "     SERVICE MENU      "
-    Write-Host "======================"
-    Write-Host "1. System Information"
-    Write-Host "2. Disk Information"
-    Write-Host "3. Restart a Computer"
-    Write-Host "`nCtrl+C or Q to exit`n"
-    $choice = Read-Host "Selection"
-    switch ($choice) {
-        1 {
-            $computers = @()
-            do {
-                $x = Read-Host "`nEnter one computer name to target"
-                if ($x -ne "") {
-                    $computers += $x
-                }
-                Get-OSInfo -ComputerName $computers
-            }
-            until ($x -eq "")
-        }
+	Write-Host "     SERVICE MENU      "
+	Write-Host "======================"
+	Write-Host "1. System Information"
+	Write-Host "2. Disk Information"
+	Write-Host "3. Restart a Computer"
+	Write-Host "`nCtrl+C or Q to exit`n"
+	$choice = Read-Host "Selection"
+	switch ($choice) {
+		1 {
+			$computers = @()
+			do {
+				$x = Read-Host "`nEnter one computer name to target"
+				if ($x -ne "") {
+					$computers += $x
+				}
+				Get-OSInfo -ComputerName $computers
+			}
+			until ($x -eq "")
+		}
 
-        2 {
-            $computers = @()
-            do {
-                $x = Read-Host "`nEnter one computer name to target"
-                if ($x -ne "") {
-                    $computers += $x
-                }
-                Get-DiskInfo -ComputerName $computers
-            }
-            until ($x -eq "")
-        }
+		2 {
+			$computers = @()
+			do {
+				$x = Read-Host "`nEnter one computer name to target"
+				if ($x -ne "") {
+					$computers += $x
+				}
+				Get-DiskInfo -ComputerName $computers
+			}
+			until ($x -eq "")
+		}
 
-        3 {
-            $computers = @()
-            do {
-                $x = Read-Host "`nEnter one computer name to target"
-                if ($x -ne "") {
-                    $computers += $x
-                }
-                Invoke-OSSHutdown -ComputerName $computers -Action Restart
-            }
-            until ($x -eq "")
-        }
-    }
+		3 {
+			$computers = @()
+			do {
+				$x = Read-Host "`nEnter one computer name to target"
+				if ($x -ne "") {
+					$computers += $x
+				}
+				Invoke-OSSHutdown -ComputerName $computers -Action Restart
+			}
+			until ($x -eq "")
+		}
+	}
 }
 While ($choice -ne "q" -or $choice -ne "Q")
--- HTMLSystemInformation.ps1
+++ HTMLSystemInformation.ps1
@@ -2,9 +2,9 @@
 
 [CmdletBinding()]
 Param (
-[ValidateCount(0,5)]
-[string[]] $ComputerName,
-[string] $Path = "C:\Scripts"
+	[ValidateCount(0, 5)]
+	[string[]] $ComputerName,
+	[string] $Path = "C:\Scripts"
 )
 
 $css = @"
@@ -21,27 +21,28 @@ th {
 
 foreach ($c in $ComputerName) {
 
-    $FilePath = Join-Path -Path $Path -ChildPath "$($c).html"
+	$FilePath = Join-Path -Path $Path -ChildPath "$($c).html"
 
-    $sys = Get-OSInfo -ComputerName $c |
-        ConvertTo-HTML -PreContent "<h2>System Info</h2>" -Fragment |
-        Out-String
+	$sys = Get-OSInfo -ComputerName $c |
+	ConvertTo-HTML -PreContent "<h2>System Info</h2>" -Fragment |
+	Out-String
 
-    $dsk = Get-DiskInfo -ComputerName $c |
-        ConvertTo-HTML -PreContent "<h2>Disk Info</h2>" -Fragment |
-        Out-String
+	$dsk = Get-DiskInfo -ComputerName $c |
+	ConvertTo-HTML -PreContent "<h2>Disk Info</h2>" -Fragment |
+	Out-String
 
-    $prc = Get-Process -ComputerName $c |
-        ConvertTo-HTML -PreContent "<h2>Processes</h2>" -Fragment |
-        Out-String
+	$prc = Get-Process -ComputerName $c |
+	ConvertTo-HTML -PreContent "<h2>Processes</h2>" -Fragment |
+	Out-String
 
-    $svc = Get-Service -ComputerName $c |
-        ConvertTo-HTML -PreContent "<h2>Services</h2>" -Fragment |
-        Out-String
+	$svc = Get-Service -ComputerName $c |
+	ConvertTo-HTML -PreContent "<h2>Services</h2>" -Fragment |
+	Out-String
 
-    $params = @{
-        "Head"="<title>Report for $c</title>$css";
-        "PreContent"="<h1>Information for $($c)</h1>";
-        "PostContent"=$sys,$dsk,$prc,$svc}
-    ConvertTo-HTML @params | Out-File -FilePath $FilePath
+	$params = @{
+		"Head"        = "<title>Report for $c</title>$css";
+		"PreContent"  = "<h1>Information for $($c)</h1>";
+		"PostContent" = $sys, $dsk, $prc, $svc
+	}
+	ConvertTo-HTML @params | Out-File -FilePath $FilePath
 }
